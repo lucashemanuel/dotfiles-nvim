e4a5b7d49820bb7b73276fb59d8b81643ffc3fb4
Lucas Hemanuel Gomes <lucashemanuel890@gmail.com>
2023-05-31 11:04:09 -0300

    new configs nvim

--- config.lua
+++ config.lua
@@ -1,11 +1,1 @@
--- Read the docs: https://www.lunarvim.org/docs/configuration
--- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
--- Forum: https://www.reddit.com/r/lunarvim/
--- Discord: https://discord.com/invite/Xb9B4Ny
-
-lvim.colorscheme = "lunar"
-lvim.format_on_save.enabled = true
-vim.opt.shiftwidth = 2        -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 2           -- insert 2 spaces for a tab
-vim.opt.relativenumber = true -- relative line numbers
-vim.opt.wrap = true           -- wrap lines
+reload("user.options")
---
+++ lua/user/options.lua
@@ -0,0 +1,50 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+lvim.colorscheme = "onedark"
+lvim.format_on_save.enabled = true
+vim.opt.shiftwidth = 2        -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 2           -- insert 2 spaces for a tab
+vim.opt.relativenumber = true -- relative line numbers
+vim.opt.wrap = true           -- wrap lines
+
+lvim.plugins = {
+  {
+    "ThePrimeagen/vim-be-good",
+    cmd = {
+      "VimBeGood",
+    },
+  },
+  { "sainnhe/sonokai" },
+  { "tanvirtin/monokai.nvim" },
+  { "navarasu/onedark.nvim" },
+  { "sainnhe/gruvbox-material" },
+  { "Mofiqul/dracula.nvim" },
+  { "cpea2506/one_monokai.nvim" },
+  { "catppuccin/nvim",          name = "catppuccin" },
+  { "dracula/vim",              name = "dracula" },
+
+  {
+    "f-person/git-blame.nvim",
+    event = "BufRead",
+    config = function()
+      vim.cmd "highlight default link gitblame SpecialComment"
+      vim.g.gitblame_enabled = 0
+    end,
+  },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+        RGB = true,      -- #RGB hex codes
+        RRGGBB = true,   -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true,   -- CSS rgb() and rgba() functions
+        hsl_fn = true,   -- CSS hsl() and hsla() functions
+        css = true,      -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true,   -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      })
+    end,
+  },
+}
