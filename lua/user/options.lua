094907ae8a5f92ad711b29cf60f451ed3627be75
Lucas Hemanuel Gomes <lucashemanuel890@gmail.com>
2023-06-14 09:22:01 -0300

    add new config structure

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -2,13 +2,16 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
-lvim.colorscheme = "onedark"
+lvim.colorscheme = "gruvbox"
 lvim.format_on_save.enabled = true
 vim.opt.shiftwidth = 2        -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 2           -- insert 2 spaces for a tab
 vim.opt.relativenumber = true -- relative line numbers
 vim.opt.wrap = true           -- wrap lines
+lvim.builtin.lualine.style = "default"
 
+
+-- Plugins Section
 lvim.plugins = {
   {
     "ThePrimeagen/vim-be-good",
@@ -16,6 +19,7 @@ lvim.plugins = {
       "VimBeGood",
     },
   },
+  { "ellisonleao/gruvbox.nvim", priority = 1000 },
   { "sainnhe/sonokai" },
   { "tanvirtin/monokai.nvim" },
   { "navarasu/onedark.nvim" },
@@ -47,4 +51,5 @@ lvim.plugins = {
       })
     end,
   },
+
 }
